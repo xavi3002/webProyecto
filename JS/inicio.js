5e0bd8fa59a251a3375917682d17c22e7d85eb4f
drodriguezs298 <drsole239@gmail.com>
2023-07-13 22:16:35 -0600

    HTML, JS, CSS INICIO

---
+++ JS/inicio.js
@@ -0,0 +1,13 @@
+/* STICKY NAVBAR SCRIPT */
+window.onscroll = function() { myFunction() };
+
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
