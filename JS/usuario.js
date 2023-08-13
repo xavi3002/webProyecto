f995bccd1eb8bfb5124cdaa5870e03bab989940b
Xavi <xavigerardo2002@gmail.com>
2023-08-13 16:45:45 -0600

    update

---
+++ JS/usuario.js
@@ -0,0 +1,42 @@
+/* 
+
+
+
+
+*/// 1- use strict
+
+const auth = firebase.auth();
+const database= firebase.database();
+
+function register(){
+    let cedula = document.getElementById("txtCedula");
+    let nombre = document.getElementById("txtNombre");
+    let apellidos = document.getElementById("txtApellidos");
+    let edad = document.getElementById("txtEdad");
+    let sexo = document.getElementById("txtSexo");
+    let direccion = document.getElementById("txtDireccion");
+    let contrasena = document.getElementById("txtContrasena");
+}
+
+
+function validate_email(email){
+    expression = /^[^@]+@\w+(\. \w+)+\w$/
+    if(expression.test(email)== true){
+        // email si sirve
+        return true;
+
+    }else{
+        return false;
+    }
+}
+
+
+function validatePassword(password){
+
+    if (password<6){
+        return false;
+    }else{
+        return true;
+    }
+
+}
