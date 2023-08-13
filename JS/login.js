941d465a743678bf3e2569f6b1ad2d2a44226678
Xavi <xavigerardo2002@gmail.com>
2023-08-13 16:48:40 -0600

    push todos

---
+++ JS/login.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDV2PGj3DyP3YuwPXH-DAw8Z-tqKrlTCw8",
+    authDomain: "proyectofinal-4c643.firebaseapp.com",
+    projectId: "proyectofinal-4c643",
+    storageBucket: "proyectofinal-4c643.appspot.com",
+    messagingSenderId: "699447371153",
+    appId: "1:699447371153:web:566a3f1995d34a5dee2630"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
